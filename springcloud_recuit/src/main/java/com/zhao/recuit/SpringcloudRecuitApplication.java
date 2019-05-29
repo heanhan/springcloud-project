5bb7ca4328155f2e8436d41b1ada5e1b48f1b7c1
heanhan <1763124707@qq.com>
2019-05-29 20:21:35 +0800

    springcloud微服务：问题-回复-模块

--- springcloud_recuit/src/main/java/com/zhao/recuit/SpringcloudRecuitApplication.java
+++ springcloud_recuit/src/main/java/com/zhao/recuit/SpringcloudRecuitApplication.java
@@ -1,13 +1,23 @@
 package com.zhao.recuit;
 
+import com.common.utils.IdWorker;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class SpringcloudRecuitApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SpringcloudRecuitApplication.class, args);
+    }
+
+    /**
+     * 雪花算法
+     */
+    @Bean
+    public IdWorker idworker(){
+        return new IdWorker();
     }
 
 }
