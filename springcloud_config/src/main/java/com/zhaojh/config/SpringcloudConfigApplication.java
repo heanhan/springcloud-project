4c6fe62958e8bd19f0331e8ae88e58fa75647a18
heanhan <1763124707@qq.com>
2019-06-28 20:23:53 +0800

    springcloud微服务:配置config

--- springcloud_config/src/main/java/com/zhaojh/config/SpringcloudConfigApplication.java
+++ springcloud_config/src/main/java/com/zhaojh/config/SpringcloudConfigApplication.java
@@ -2,10 +2,16 @@ package com.zhaojh.config;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+/**
+ * @EnableEurekaClient  标注为eureka的客户端
+ * @EnableConfigServer  开启config的服务
+ */
 @SpringBootApplication
 @EnableEurekaClient
+@EnableConfigServer
 public class SpringcloudConfigApplication {
 
     public static void main(String[] args) {
