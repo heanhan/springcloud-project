56d916e15d2ed2e1470f5cd534be9527774cb98e
heanhan <1763124707@qq.com>
2019-09-21 17:47:35 +0800

    springcloud微服务： 修改springcloud-sms 的短信模块。。。。。。。。

--- springcloud_qa/src/main/java/com/zhaojh/qa/controller/ReplyController.java
+++ springcloud_qa/src/main/java/com/zhaojh/qa/controller/ReplyController.java
@@ -1,10 +1,23 @@
 package com.zhaojh.qa.controller;
 
 
+import com.google.common.collect.Lists;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.swing.*;
 
 @RestController
 @RequestMapping(value="/reply")
 public class ReplyController {
+
+    @GetMapping(value = "/test")
+    public String test(){
+        Lists.newArrayList("","");
+
+
+        return null;
+
+    }
 }
